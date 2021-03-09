31079028194547c9c86ea774337b4a791a0b35c8
DanielMCGR <73974457+DanielMCGR@users.noreply.github.com>
2021-03-09 17:22:00 +0000

    Updated SolveBacktracking.java
    Cleaned unnecessary comments and added basic JavaDoc for easier understanding in the future.

--- src/SolveBacktracking.java
+++ src/SolveBacktracking.java
@@ -1,79 +1,74 @@
-import java.util.*;
+/**
+ * This class uses a much better and simpler
+ * method for solving a Sudoku puzzle, via
+ * backtracking method.
+ *
+ * @author      Daniel Rocha
+ * @version     1.0
+ * @see Sudoku
+ */
 
 public class SolveBacktracking {
 
-    public static void main() {
-        //this is a copy of the code from AutoSolver
-        //Main.board=new int[81];
-        //Main.board = Main.GetBoard("040805200020040050500000004090003120106078003370904080000006700008359010019007600");
-        //Main.isDefault = Main.SetDefault();
-        //System.out.print(Arrays.toString(isDefault)); //To make sure the command is working properly
-        //System.out.println("This is the base board");
-        //Main.printBoard();
-        //System.out.println(" ");
-        //System.out.println(" ");
-        //System.out.println("This is the completed board");
-        //if (SolveBacktracking(Main.board))
-        //{
-        //    Main.printBoard();
-        //}
-        //System.out.println(" ");
-        //this is just to check if it is the same as the manual solution
-        //System.out.println("[9, 4, 7, 8, 3, 5, 2, 6, 1, 6, 2, 3, 7, 4, 1, 8, 5, 9, 5, 8, 1, 6, 9, 2, 3, 7, 4, 8, 9, 4, 5, 6, 3, 1, 2, 7, 1, 5, 6, 2, 7, 8, 9, 4, 3, 3, 7, 2, 9, 1, 4, 5, 8, 6, 4, 3, 5, 1, 2, 6, 7, 9, 8, 7, 6, 8, 3, 5, 9, 4, 1, 2, 2, 1, 9, 4, 8, 7, 6, 3, 5]".equals(Arrays.toString(Main.board)));
-        SolveBacktracking(Main.board);
-    }
-
-    public static boolean SolveBacktracking(int[] board) {
+    /**
+     * Solves a Sudoku board using Backtracking
+     *
+     * @param board the board to be solved
+     * @return true if the board is completed, and false if it is not
+     */
+    public static boolean solveBacktracking(int[] board) {
         boolean isDone = true;
         int pos = -1;
         for (int i = 0; i < 81; i++) {
-            if (Main.board[i]==0) {
+            if (Sudoku.board[i]==0) {
                 pos = i;
                 isDone = false;
                 break;
             }
         }
-        if (isDone) {  return true;    }
-            for (int Val = 1; Val <= 9; Val++)
-            {
-                if (IsPossible(pos, Val)) {
-                    Main.board[pos] = Val;
-                    if (SolveBacktracking(Main.board)) {
-                        return true;
-                    } else {
-                        Main.board[pos] = 0;
-                    }
+        if (isDone) return true;
+        for (int Val = 1; Val <= 9; Val++) {
+            if (isPossible(pos, Val)) {
+                Sudoku.board[pos] = Val;
+                if (solveBacktracking(Sudoku.board)) {
+                    return true;
+                } else {
+                    Sudoku.board[pos] = 0;
                 }
             }
+        }
         return false;
     }
 
+    /**
+     * Checks if it is possible to fill a given value
+     * onto the selected position.
+     * <p>
+     * (Based on AutoSolver.possibleNumber)
+     *
+     * @param pos the position to be checked
+     * @param val the value to be checked
+     * @return whether or not the value is valid
+     */
     //based on AutoSolver class PossibleNumber function
-    public static boolean IsPossible(int pos, int Val) {
+    public static boolean isPossible(int pos, int val) {
         boolean possible = true;
-        int Col = AutoSolver.BaseCol(pos);
-        int Row = AutoSolver.BaseRow(pos);
-        int Corner = AutoSolver.CornerOfBlock(pos);
+        int col = AutoSolver.columnNum(pos);
+        int row = AutoSolver.rowNum(pos);
+        int corner = AutoSolver.cornerOfBlock(pos);
         //check the column
         for (int i = 0; i < 9; i++) {
-            if (Val == Main.board[Col + (i * 9)]) {
-                possible = false;
-            }
-        }
+            if (val == Sudoku.board[col + (i * 9)]) {
+                possible = false; } }
         //check the row
         for (int i = 0; i < 9; i++) {
-            if (Val == Main.board[(Row * 9) + i]) {
-                possible = false;
-            }
-        }
+            if (val == Sudoku.board[(row * 9) + i]) {
+                possible = false; } }
         //and last, check the block (starting from its top left)
         for (int i = 0; i < 3; i++) {    //row
             for (int j = 0; j < 3; j++) {        //column
-                if (Val == Main.board[(j * 9) + Corner + i]) {
-                    possible = false;
-                }
-            }
-        }
+                if (val == Sudoku.board[(j * 9) + corner + i]) {
+                    possible = false; } } }
         return possible;
     }
 }
