ce11ccdea1ff9f0a0399ca9ec8a8870243271774
DanielMCGR <73974457+DanielMCGR@users.noreply.github.com>
2021-03-09 17:15:08 +0000

    Updated and renamed Main to Sudoku
    Reviewed the code for the Sudoku Class (the main class), removing the "help" feature, as most the interface is very simple and understandable, and allowed for choosing difficulty (the amount of possible boards is now 3, but you can get more from BoardList.txt).
    Added basic JavaDoc for easier understanding in the future.

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,6 +1,23 @@
 import java.util.*;
 
-public class Main {
+/**
+ * This is a Sudoku game. While it is fully playable,
+ * the main purpose is to use the implemented auto-solvers
+ * (one of them using regular solving methods and
+ * the other one using a backtracking method.
+ * <p>
+ * The default boards and the ones you can input have
+ * the same format, a string composed of 81 integers,
+ * each representing what is the value of each of the
+ * 81 squares in a Sudoku board (0 means an empty square)
+ *
+ * @author      Daniel Rocha
+ * @version     1.0
+ * @see AutoSolver
+ * @see SolveBacktracking
+ */
+
+public class Sudoku {
     static Scanner input;
     static int[] board;
     static boolean[] isDefault;
@@ -18,25 +35,33 @@ public class Main {
         boolean isAuto = false;
 
         //This is for when the game starts
-        System.out.println("Welcome to the sudoku game. You can type help at any time to get help.");
         while(!valid){
             System.out.println();
-            System.out.println("Would you like to use a default board or input one yourself? (type either default or input)");
+            System.out.println("Would you like to use a default board or input one yourself? (type either easy, medium, hard (these are the default boards) or input)");
             String first_input =input.nextLine().toLowerCase();
             switch (first_input) {
-                case "help" -> help();
-                case "default" -> {
-                    board = GetBoard("040805200020040050500000004090003120106078003370904080000006700008359010019007600");
+                case "easy" -> {
+                    board = getBoard("040805200020040050500000004090003120106078003370904080000006700008359010019007600");
                     printBoard();
                     valid=true;
-
                     }
+                case "medium" -> {
+                    board = getBoard("009750000000000000005382000010000003002000908406000000900040130700006549000200000");
+                    printBoard();
+                    valid=true;
+                }
+                case "hard" -> {
+                    board = getBoard("400000000000009000000000785007048050001300000006070000860000903700005062003700000");
+                    printBoard();
+                    valid=true;
+                }
+                //there are also several other boards available in BoardList.txt
                 case "input" -> {
                     while(!valid) {
                         System.out.println("Please type the numbers on the board (From top left to bottom right, if there is a blank space count it as a zero):");
                         String input_board =input.nextLine().toLowerCase();
-                        if(ValidBoard(input_board)) {
-                            board = GetBoard(input_board);
+                        if(isValidBoard(input_board)) {
+                            board = getBoard(input_board);
                             printBoard();
                             valid=true;
                         } else if(input_board.equals("leave")){
@@ -50,32 +75,31 @@ public class Main {
                 }
                 default -> {
                     System.out.println("That command is not recognized, here is a list of commands you can input:");
-                    System.out.println("Help; Default; Input");
+                    System.out.println("Easy; Medium; Hard; Input");
                 }
             }
         }
-
         System.out.println();
-        System.out.println("You can type 'start' to begin the game or example to see an example board (with the position numbers)");
+        System.out.println("You can type 'start' to begin the game, 'example' to see an example board (with the position numbers)");
+        System.out.println("and autosolve1(base auto-solver) or autosolve2 (using backtracking) to auto-solve the board!");
         while (!start) { //commands before the beginning of the game
-            System.out.println("Please input a command:");
+            System.out.println("Please input a command :");
             String first_input =input.nextLine().toLowerCase();
             switch (first_input) {
-                case "help" -> help();
                 case "start" -> start = true;
                 case "example" -> printExampleBoard();
                 case "board" -> printBoard();
-                case "autosolve1" -> {AutoSolver.main(); isAuto=true; start=true;}
-                case "autosolve2" -> {SolveBacktracking.main(); isAuto=true; start=true;}
+                case "autosolve1" -> {AutoSolver.solve(); isAuto=true; start=true;}
+                case "autosolve2" -> {SolveBacktracking.solveBacktracking(board); isAuto=true; start=true;}
                 default -> {
                     System.out.println("That command is not recognized, here is a list of commands:");
-                    System.out.println("Help; Start; Example; Board; AutoSolve1; AutoSolve2");
+                    System.out.println("Start; Example; Board; AutoSolve1; AutoSolve2");
                 }
             }
         }
 
         //The booleans for the default positions are generated, the board has already been set by the user
-        isDefault = SetDefault();
+        isDefault = setDefault();
         //System.out.print(Arrays.toString(isDefault)); //To make sure the command is working properly
         System.out.println();
 
@@ -103,7 +127,7 @@ public class Main {
                     System.out.println("The value for position " + pos + " has been changed to " + num);
                 }
             }
-            if(BoardIsComplete(board)) {
+            if(isBoardComplete(board)) {
                 win=true;
                 System.out.println("Congratulations. You Won!");
                 //System.out.println("Final Board: "+Arrays.toString(board));
@@ -121,43 +145,89 @@ public class Main {
 
     }
 
-    public static boolean ValidBoard(String board){
-        return GetBoard(board).length == 81 && isInteger(board);
+    /**
+     * Checks if a board is valid (first check if the given string
+     * can be converted to an int, and if it can, if it's length is
+     * 81 (default board size))
+     * 
+     * @param board the board to be checked (using the standard format)
+     * @return whether or not the board is valid
+     */
+    public static boolean isValidBoard(String board){
+        try {
+            Integer.parseInt(board);
+        }
+        catch( Exception e ) {
+            return false;
+        }
+        return getBoard(board).length == 81;
     }
 
-    public static boolean BoardIsComplete(int[] board){
+    /**
+     * Checks if the given board is complete, by checking
+     * if both all columns and all rows are complete
+     * (by complete it means to have no zeros, or that
+     * all the slots are filled)
+     *
+     * @param board the board to be checked (as an int array)
+     * @return whether or not the board is complete
+     * @see #isRowComplete(int, int[])
+     * @see #isColumnComplete(int, int[])
+     */
+    public static boolean isBoardComplete(int[] board){
         boolean isComplete=true;
         for (int i = 0; i < 9; i++){
-            if(!RowIsComplete(i, board)){
+            if(!isRowComplete(i, board)){
                 isComplete=false;
             }
-            if(!ColumnIsComplete(i, board)){
+            if(!isColumnComplete(i, board)){
                 isComplete=false;
             }
         }
         return isComplete;
     }
 
-    public static boolean RowIsComplete(int num, int[] board){
-        int soma=0;
+    /**
+     * Checks if a selected row is complete
+     *
+     * @param num the number of the row to be checked
+     * @param board the board to be checked (as an int array)
+     * @return whether or not the selected row is complete
+     */
+    public static boolean isRowComplete(int num, int[] board){
+        int sum=0;
         for (int i = 0; i < 9; i++){
-            soma=board[i+(num*9)]+soma;
+            sum=board[i+(num*9)]+sum;
         }
-        return soma==45; //adding all numbers from 1 to 9 = 45
-    }
-    public static boolean ColumnIsComplete(int num, int[] board){
-        int soma=0;
-        for (int i = 0; i < 9; i++){
-            soma=board[i*9+(num)]+soma;
-        }
-        return soma==45; //adding all numbers from 1 to 9 = 45
+        return sum==45; //adding all numbers from 1 to 9 = 45
     }
 
-    //Simple mechanism to input a board, by typing all numbers from top left to bottom right in order (if there is no number, it's set as 0 (which can't be used in sudoku )
-    //since this isn't a fast or reliable method, it will just be used for testing
-    //example: 040805200020040050500000004090003120106078003370904080000006700008359010019007600
-    //Final Board: [9, 4, 7, 8, 3, 5, 2, 6, 1, 6, 2, 3, 7, 4, 1, 8, 5, 9, 5, 8, 1, 6, 9, 2, 3, 7, 4, 8, 9, 4, 5, 6, 3, 1, 2, 7, 1, 5, 6, 2, 7, 8, 9, 4, 3, 3, 7, 2, 9, 1, 4, 5, 8, 6, 4, 3, 5, 1, 2, 6, 7, 9, 8, 7, 6, 8, 3, 5, 9, 4, 1, 2, 2, 1, 9, 4, 8, 7, 6, 3, 5]
-    public static int[] GetBoard(String conv) {
+    /**
+     * Checks if a selected column is complete
+     *
+     * @param num the number of the column to be checked
+     * @param board the board to be checked (as an int array)
+     * @return whether or not the selected column is complete
+     */
+    public static boolean isColumnComplete(int num, int[] board){
+        int sum=0;
+        for (int i = 0; i < 9; i++){
+            sum=board[i*9+(num)]+sum;
+        }
+        return sum==45; //adding all numbers from 1 to 9 = 45
+    }
+
+    /**
+     * Converts a string representing a board into an
+     * int array, to later be used by other methods
+     *
+     * @param conv the String representing the array (using the standard format)
+     * @return the converted int array
+     */
+    //example String: 040805200020040050500000004090003120106078003370904080000006700008359010019007600
+    //example Array: [0, 4, 0, 8, 0, 5, 2, 0, 0, 0, 2, 0, 0, 4, 0, 0, 5, 0, 5, 0, 0, 0, 0, 0, 0, 0, 4, 0, 9, 0, 0, 0, 3, 1, 2, 0, 1, 0, 6, 0, 7, 8, 0, 0, 3, 3, 7, 0, 9, 0, 4, 0, 8, 0, 0, 0, 0, 0, 0, 6, 7, 0, 0, 0, 0, 8, 3, 5, 9, 0, 1, 0, 0, 1, 9, 0, 0, 7, 6, 0, 0]
+    //Final Board (Solved): [9, 4, 7, 8, 3, 5, 2, 6, 1, 6, 2, 3, 7, 4, 1, 8, 5, 9, 5, 8, 1, 6, 9, 2, 3, 7, 4, 8, 9, 4, 5, 6, 3, 1, 2, 7, 1, 5, 6, 2, 7, 8, 9, 4, 3, 3, 7, 2, 9, 1, 4, 5, 8, 6, 4, 3, 5, 1, 2, 6, 7, 9, 8, 7, 6, 8, 3, 5, 9, 4, 1, 2, 2, 1, 9, 4, 8, 7, 6, 3, 5]
+    public static int[] getBoard(String conv) {
         int[] num = new int[conv.length()];
         for (int i = 0; i < conv.length(); i++){
             num[i] = conv.charAt(i) - '0';
@@ -165,20 +235,15 @@ public class Main {
         return num;
     }
 
-    //used to make sure the input string can be converted to an int and to avoid errors, might delete later
-    public static boolean isInteger( String input ) {
-        try {
-            Integer.parseInt( input );
-            return true;
-        }
-        catch( Exception e ) {
-            return false;
-        }
-    }
-
+    /**
+     * Creates a boolean array that stores the default positions (one of the "starting" ones,
+     * that cant be changed), to be used on the isDefault variable.
+     *
+     * @return boolean array with the default positions
+     */
     //the array isDefault stores whether or not the number in a position was one of the "starting" ones, that cant be changed
     //made slight changes to this so it would work easily in other classes
-    public static boolean[] SetDefault() {
+    public static boolean[] setDefault() {
         boolean[] list = new boolean[81];
         for (int i = 0; i < 81; i++){
             if(board[i]==0){
@@ -191,7 +256,9 @@ public class Main {
         return list;
     }
 
-    //prints the board according to the board array
+    /**
+     * Prints the board according to the board array
+     */
     static void printBoard() {
         System.out.println("This is the current state of the board:");
         System.out.println();
@@ -216,7 +283,9 @@ public class Main {
         System.out.println("└---┴---┴---┴---┴---┴---┴---┴---┴---┘");
     }
 
-    //just an example board useful for understanding where each array position is
+    /**
+     * Prints an example board useful for understanding where each array position is
+     */
     static void printExampleBoard() {
         System.out.println("This is the example board:");
         System.out.println();
@@ -239,8 +308,5 @@ public class Main {
         System.out.println("|----┼----┼----┼----┼----┼----┼----┼----┼----|");
         System.out.println("| 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80 |");
         System.out.println("└----┴----┴----┴----┴----┴----┴----┴----┴----┘");
-        }
-    static void help() {
-        System.out.println("The help command is under construction");
     }
 }
